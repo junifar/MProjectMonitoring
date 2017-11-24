f87d7d328ef0d07e997aa3cf37f589a4e30f8daa
blueaintz <sepedanenek21@gmail.com>
2017-11-24 18:02:49 +0700

    sign up activity error

--- app/src/main/java/com/prasetia/mprojectmonitoring/SignupActivity.kt
+++ app/src/main/java/com/prasetia/mprojectmonitoring/SignupActivity.kt
@@ -2,11 +2,26 @@ package com.prasetia.mprojectmonitoring
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_signup.*
+import retrofit2.adapter.rxjava2.Result
 
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        btnSignin.setOnClickListener(
+                Toast.makeText(applicationContext.f"berhasil",Toast.LENGTH_SHORT).show();
+        )
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.signup, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
