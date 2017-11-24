aff7246c115b933d8ed187a13945fb09f4e2dfc0
junifar <junifar@gmail.com>
2017-11-24 17:56:54 +0700

    RecyclerView : Adding Feature
    ClickEvent

--- app/src/main/java/com/prasetia/mprojectmonitoring/adapter/ProjectCorrectiveAdapter.kt
+++ app/src/main/java/com/prasetia/mprojectmonitoring/adapter/ProjectCorrectiveAdapter.kt
@@ -3,6 +3,7 @@ package com.prasetia.mprojectmonitoring.adapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.prasetia.mprojectmonitoring.R
 import com.prasetia.mprojectmonitoring.config.inflate
 import com.prasetia.mprojectmonitoring.pojo.ProjectCorrective
@@ -12,8 +13,6 @@ import kotlinx.android.synthetic.main.item_project_corrective.view.*
  * Created by prasetia on 11/24/2017.
  */
 class ProjectCorrectiveAdapter(private val items: List<ProjectCorrective>): RecyclerView.Adapter<ProjectCorrectiveAdapter.ViewHolder>() {
-
-
 
     override fun onBindViewHolder(holder: ProjectCorrectiveAdapter.ViewHolder, position: Int) {
         val item = items[position]
@@ -28,9 +27,19 @@ class ProjectCorrectiveAdapter(private val items: List<ProjectCorrective>): Recy
 
     override fun getItemCount(): Int = items.size
 
-    class ViewHolder(v: View?) : RecyclerView.ViewHolder(v) {
+    class ViewHolder(v: View?) : RecyclerView.ViewHolder(v), View.OnClickListener {
+        private var item: ProjectCorrective? = null
+        init {
+            v?.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View?) {
+            Toast.makeText(itemView.context, item?.iD.toString(), Toast.LENGTH_SHORT).show()
+        }
+
         private var view: View? = v
         fun bindItem(item: ProjectCorrective){
+            this.item = item
             view?.textViewName!!.text = item.iD.toString()
         }
     }
