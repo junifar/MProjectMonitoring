ff92900f1c7c15056425d779e4eb967aa2f1be2a
blueaintz <sepedanenek21@gmail.com>
2017-12-08 10:50:46 +0700

    sign up activity error1

--- app/src/main/java/com/prasetia/mprojectmonitoring/SignupActivity.kt
+++ app/src/main/java/com/prasetia/mprojectmonitoring/SignupActivity.kt
@@ -1,29 +1,70 @@
 package com.prasetia.mprojectmonitoring
 
 import android.os.Bundle
+import android.support.design.widget.TextInputEditText
+import android.support.design.widget.TextInputLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
-import retrofit2.adapter.rxjava2.Result
+import android.widget.Toast
+import android.widget.EditText
+import java.util.jar.Attributes
+
 
 class SignupActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 
-        btnSignin.setOnClickListener(
-                {
-                    Toast.makeText(applicationContext, "berhasil", Toast.LENGTH_SHORT).show()
-                }
-        )
+
+
+
+
+        btnSignin.setOnClickListener({
+
+            if (txtName.text.toString()) {
+                txtName.setError("Username diperlukan!");
+                return@setOnClickListener
+            }
+        })
+            /*if(!checkLength(txtEmail.text.toString())) {
+                displayError(txtEmail, txtLayoutEmail, message = "Email Diperlukan")
+                return@setOnClickListener
+            }
+            if(!checkLength(txtPassword.text.toString())) {
+                displayError(txtPassword, txtLayoutPassword, message = "password Diperlukan")
+                return@setOnClickListener
+            }
+
+
+        })
+
+    }
+        fun displayError(v: TextInputEditText, v2: TextInputLayout, message: String) {
+            v.setError(message)
+            v2.isErrorEnabled = true
+        }
+
+        fun checkLength(value1: String): Boolean {
+            return value1.isNotEmpty()
+        }
+
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+            menuInflater.inflate(R.menu.signup, menu)
+            return super.onCreateOptionsMenu(menu)
+        }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.signup, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-}
+//
+//protected fun onCreate(savedInstanceState: Bundle) {
+//    super.onCreate(savedInstanceState)
+//    setContentView(R.layout.activity_main)
+//    ButterKnife.bind(this)
+//
+//    validator = Validator(this)
+//    validator.setValidationListener(this)
+//}
